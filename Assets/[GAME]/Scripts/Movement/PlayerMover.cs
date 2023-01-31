075ce5e70d72270d276227a3e460014e74fb83aa
Caglar <mcaglarcnkr@gmail.com>
2023-01-31 15:59:10 +0300

    stickman, platform models

---
+++ Assets/[GAME]/Scripts/Movement/PlayerMover.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// drag control for player
+/// </summary>
+
+public class PlayerMover : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+
+        }
+    }
+}
